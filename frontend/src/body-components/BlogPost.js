5f72828cb70e49b9e067babedcea59c6b524694f
jasongilmore <32154076+JasonGilmore@users.noreply.github.com>
2018-08-30 19:37:49 +1000

    Blog post use state for mock data

--- frontend/src/body-components/BlogPost.js
+++ frontend/src/body-components/BlogPost.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import '../styles/BlogPost.css';
 
+/* 
+    Blog Post cards will be displayed on the main page
+    they will display a title, image where it is available
+    and a summary or short blurb of the blog content.
+*/
+
 class BlogPost extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: 'Blog Post',
+            body: 'This is some sample blog post body content. It will display the first few sentences of the post.'
+        }
+    }
+
     render() {
         return (
             <div className="blog-post">
-                Blog Post
+                {this.state.title}
                 <br></br>
                 <br></br>
-                This is some sample blog post body content. It will display the first few sentences of the post.
+                {this.state.body}
             </div>
         )
     }
--- frontend/src/body-components/BlogPosts.js
+++ frontend/src/body-components/BlogPosts.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import '../styles/BlogPosts.css';
 import BlogPost from './BlogPost.js';
 
+/*
+    BlogPosts component is simply a container holding
+    the individual BlogPost(s) and divides them up into a card view
+    for the main page of our website.
+*/
+
 class BlogPosts extends Component {
     render() {
         return (
