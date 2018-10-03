fb3d896d0ec5bdd071a0e2f2959d1ac84f7c9475
jasongilmore <32154076+JasonGilmore@users.noreply.github.com>
2018-10-03 12:34:11 +1000

    Add create blog post alerts

--- frontend/src/body-components/CreateBlog.js
+++ frontend/src/body-components/CreateBlog.js
@@ -11,10 +11,12 @@ class CreatePost extends Component {
         super();
         this.state = {
             title: '',
-            content: ''
+            content: '',
+            alert: ''
         }
         this.newBlog = this.newBlog.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.dismissAlert = this.dismissAlert.bind(this);
     }
     
     newBlog(event) {
@@ -34,25 +36,47 @@ class CreatePost extends Component {
                 });
                 this.props.history.push("/myBlogs");
             } else if (res.status === 403) {
-                console.log('Error creating new blog post. Access denied')
+                this.setState({
+                    alert: 'Error creating new blog post. Access denied'
+                });
             } else {
-                console.log('Error creating new blog post');
+                this.setState({
+                    alert: 'Error creating new blog post'
+                });
             }
         })
         .catch(err => console.error('An error occurred: ' + err));
     }
+
+    dismissAlert() {
+        this.setState({
+            alert: ''
+        });
+    }
+
     handleInputChange(event) {
         const target = event.target;
         const name = target.name;
         this.setState({
-            [name]: target.value
+            [name]: target.value,
+            // Hide alert when user changes input
+            alert: ''
         });
     }
 
     render() {
         return (
             <div>
+                {/* Alert for incorrect blog post */}
+                {this.state.alert ? (
+                    <div className="alert alert-danger alert-dismissible">
+                        {this.state.alert}
+                        <button type="button" onClick={this.dismissAlert} className="close">&times;</button>
+                    </div>
+                ) : ('')}
+
                 <h1>New Blog Post</h1>
+                {/* Blog post form */}
                 <form onSubmit={this.newBlog}>
                     <input className="create-post-input" name="title" value={this.state.title} onChange={this.handleInputChange} type="text" placeholder="Title" required></input>
                     <textarea rows="10" cols="50" name="content" value={this.state.content} onChange={this.handleInputChange} className="create-post-input" placeholder="Blog post content" required></textarea>
