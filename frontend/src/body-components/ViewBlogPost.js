b1743e9b59f34272b3f1f982587799f008a40b2d
Kevin Tang <kevintangk.t@hotmail.com>
2018-10-09 18:06:11 +1100

    View blog styling

--- frontend/src/body-components/ViewBlogPost.js
+++ frontend/src/body-components/ViewBlogPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Loading from '../Loading'
-import { EditorState, convertFromRaw, Editor, ContentState} from 'draft-js';
+import { EditorState, convertFromRaw, Editor, ContentState } from 'draft-js';
 import '../styles/ViewBlogPost.css'
 
 class ViewBlogPost extends Component {
@@ -36,7 +36,7 @@ class ViewBlogPost extends Component {
     // This avoids it flashing on screen briefly before content loads
     startLoading() {
         if (this.state.isLoading) {
-            this.setState({showLoading: true});
+            this.setState({ showLoading: true });
         }
         clearInterval(this.timer);
     }
@@ -47,7 +47,7 @@ class ViewBlogPost extends Component {
 
     componentDidMount() {
         let path = this.props.location.pathname;
-        let blogId = this.props.location.pathname.substr(path.length-1)
+        let blogId = this.props.location.pathname.substr(path.length - 1)
         fetch('/blog/' + blogId)
             .then(res => {
                 this.setState({
@@ -85,7 +85,7 @@ class ViewBlogPost extends Component {
                     });
                 }
             }
-        )
+            )
     }
 
     dismissAlert() {
@@ -190,53 +190,58 @@ class ViewBlogPost extends Component {
     render() {
         return (
             <div>
-            <button className="btn btn-danger" onClick={this.props.history.goBack}>Back</button>
-            {this.state.showLoading ? (
-                <Loading />
-            ) : (
-                    this.state.isLoading ? ('') : (
-                        <div>
-                            {/* Alert for error loading blog post */}
-                            {this.state.alert ? (
-                                <div className="alert alert-danger alert-dismissible">
-                                    {this.state.alert}
-                                    <button type="button" onClick={this.dismissAlert} className="close">&times;</button>
+                <div className="view-blog-backBtn">
+                    <button className="btn btn-danger" onClick={this.props.history.goBack}>Back</button>
+                </div>
+                {this.state.showLoading ? (
+                    <Loading />
+                ) : (
+                        this.state.isLoading ? ('') : (
+                            <div>
+                                <div className="view-blog-alert">
+                                    {/* Alert for error loading blog post */}
+                                    {this.state.alert ? (
+                                        <div className="alert alert-danger alert-dismissible">
+                                            {this.state.alert}
+                                            <button type="button" onClick={this.dismissAlert} className="close">&times;</button>
+                                        </div>
+                                    ) : ('')}
                                 </div>
-                            ) : ('')}
 
-                            <h1>{this.state.title}</h1>
-                            <h2>By {this.state.author}</h2>
-                            <hr></hr>
-                            <p>{this.state.description}</p>
-                            <div className="editor-display">
-                                {<Editor editorState={this.state.editorState} readOnly />}
+                                <h1 className="view-blog-title">{this.state.title}</h1>
+                                <h3 className="view-blog-author" label="By:"> {this.state.author}</h3>
+                                <h6 className="view-blog-description">{this.state.description}</h6>
+
+                                <hr className="view-blog-hr"></hr>
+                                <div className="editor-display">
+                                    {<Editor editorState={this.state.editorState} readOnly />}
+                                </div>
+
+                                {/* Likes. If no likes, show 0 */}
+                                    <p>Likes: {this.state.likes ? this.state.likes : 0}</p>
+
+                                {this.props.isLoggedIn ? (
+                                    this.state.liked ? (
+                                        <button className="btn btn-primary" onClick={this.likeBlog}>Unlike</button>
+                                    ) : (
+                                            <button className="btn btn-primary" onClick={this.likeBlog}>Like</button>
+                                        )
+                                ) : ('')}
+                                {/* Comments. If no comments, show 0 */}
+                                <p>Comments: {this.state.commentCount ? this.state.commentCount : 0}</p>
+
+                                <h2>Comments</h2>
+                                {this.state.comments.map(comment => this.eachComment(comment))}
+                                {/* Comment form */}
+                                <p>Comment:</p>
+                                <form className="col-sm-9 col-md-7 col-lg-5 mx-auto" onSubmit={this.comment}>
+                                    <input className="form-control" placeholder="Comment" value={this.state.inputComment} onChange={this.handleInputChange} required></input>
+                                    <button className="btn btn-primary" type="submit">Submit</button>
+                                </form>
+
                             </div>
-
-                             {/* Likes. If no likes, show 0 */}
-                            <p>Likes: {this.state.likes ? this.state.likes : 0}</p>
-
-                            {this.props.isLoggedIn ? (
-                                this.state.liked ? (
-                                    <button className="btn btn-primary" onClick={this.likeBlog}>Unlike</button>
-                                ) : (
-                                        <button className="btn btn-primary" onClick={this.likeBlog}>Like</button>
-                                    )
-                            ) : ('')}
-                            {/* Comments. If no comments, show 0 */}
-                            <p>Comments: {this.state.commentCount ? this.state.commentCount : 0}</p>
-
-                            <h2>Comments</h2>
-                            {this.state.comments.map(comment => this.eachComment(comment))}
-                            {/* Comment form */}
-                            <p>Comment:</p>
-                            <form className="col-sm-9 col-md-7 col-lg-5 mx-auto" onSubmit={this.comment}>
-                                <input className="form-control" placeholder="Comment" value={this.state.inputComment} onChange={this.handleInputChange} required></input>
-                                <button className="btn btn-primary" type="submit">Submit</button>
-                            </form>
-                            
-                        </div>
-                    )
-                )}
+                        )
+                    )}
             </div>
         )
     }
