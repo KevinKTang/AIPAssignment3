3208a446df6c358a912b2ee6e83861b1492871c7
jasongilmore <32154076+JasonGilmore@users.noreply.github.com>
2018-10-10 18:25:18 +1100

    Update regex for email validation to be more precise

--- backend/Server.js
+++ backend/Server.js
@@ -391,7 +391,7 @@ app.post('/newUser', (req, res) => {
         res.status(400).send({alert: 'Firstname must be 2 or more characters in length.'});
     } else if (!req.body.lastname || req.body.lastname.length < 2) {
         res.status(400).send({alert: 'Lastname must be 2 or more characters in length.'});
-    } else if (!(/.{1,}@{1}.{1,}\.{1,}.{1,}/.test(req.body.email))) {
+    } else if (!(/[\w+.]+@((\w[\w-]*)\.)+(\w[\w-]*)/.test(req.body.email))) {
         res.status(400).send({alert: 'Email format is incorrect. It must be in a format similar to example@email.com'});
     } else if (!req.body.password || req.body.password.length < 8) {
         res.status(400).send({alert: 'Password must be 8 or more characters in length.'});
