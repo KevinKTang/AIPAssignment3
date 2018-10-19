318e569a36d4836b1a9861326fbb02339f3ce1a4
jasongilmore <32154076+JasonGilmore@users.noreply.github.com>
2018-10-19 23:12:43 +1100

    Remove unused packages

--- backend/Server.js
+++ backend/Server.js
@@ -12,18 +12,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(session({
-    // In production, change secret randomly generated string from an environment variable
     secret: 'super secret',
     genid: function (req) {
         // Use UUIDs for session IDs
         return uuidv4();
     },
     saveUninitialized: false,
-    /*
-    resave: true // Come back to this when sorted out store
-    app.set('trust proxy', 1) // Come back to this
-    secure: true // For https enabled websites
-    resave: true, // If session store doesn't implement touch command */
 }));
 
 var models = require('./models');
@@ -40,12 +34,7 @@ var server = http.createServer(app);
 //app.use((req, res, next) => setTimeout(() => next(), 2000));
 
 // Set up the tables
-models.sequelize.sync().then(() => {
-    // Listen on port
- //   server.listen(5000, () => {
- //       console.log('Express server listening on port 5000');
- //   });
-});
+models.sequelize.sync();
 
 server = app.listen(5000, () => {
     console.log('Running on http://localhost:5000/');
